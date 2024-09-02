01729b79065ea158439222fdc4aaae5bded1598e
TriboRibo <roman.tribockij@gmail.com>
2024-09-02 13:07:35 +0300

    ver 0.8

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,3 +1,5 @@
+const User = require('../schemas/userSchema');
+
 module.exports = {
 	registerValidation: (req, res, next) => {
 		const {name, password, repeatPassword} = req.body
@@ -27,6 +29,41 @@ module.exports = {
 		const {name, password} = req.body
 		if (!name || !password) {
 			return res.status(400).json({error: 'All field are required.'})
+		}
+		next()
+
+	},
+	validateUsername: (req, res, next) => {
+		const {name} = req.body
+		if (name.length < 4 || name.length > 20) {
+			return res.status(400).json({error: 'Username must be between 4 and 20 characters.'})
+		}
+		if (!name || typeof name !== "string")  {
+			return res.status(400).json({success: false, error: 'Invalid username'})
+		}
+		next()
+	},
+	validatePassword: (req, res, next) => {
+		const {newPassword} = req.body
+		const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*_+])[A-Za-z\d!@#$%^&*_+]{4,20}$/
+		if (!newPassword || !passwordRegex.test(newPassword)) {
+			return res.status(400).json({
+				error: 'Password must be 4-20 characters long, include at least one uppercase letter, one lowercase letter, and one special character (!@#$%^&*_+), and may contain digits.'
+			})
+		}
+		next()
+	},
+	validateAvatar: (req, res, next) => {
+		const {newAvatar} = req.body
+		const urlRegex = /^(https?:\/\/)[\w\-]+(\.[\w\-]+)+[/#?]?.*$/
+		if (!newAvatar || typeof newAvatar !== 'string' || !urlRegex.test(newAvatar)) {
+			return res.status(400).json({error: 'Avatar must be a valid string URL.'})
+		}
+		const validImage = ['.jpg', '.jpeg', '.png', '.gif', '.svg', '.bmp', '.webp']
+		const urlLowerCase = newAvatar.toLowerCase()
+		const hasValidExtension = validImage.some(ext => urlLowerCase.endsWith(ext))
+		if (!hasValidExtension) {
+			return res.status(400).json({error: 'Avatar URL must point to a valid image format (e.g., .jpg, .png).'})
 		}
 		next()
 	}
--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -6,24 +6,34 @@ const {
 	logIn,
 	getAllUsers,
 	getSingleUser,
-	sendPublicMessage,
-	getPublicMessage,
+	// sendPublicMessage,
+	// getPublicMessage,
 	// sendMessageToUser,
 	// receiveMessageFromUser
+	changeUsername,
+	changePassword,
+	changeAvatar
 } = require('../controllers/mainController')
 
 const {
 	registerValidation,
 	logInValidation,
+	validateUsername,
+	validatePassword,
+	validateAvatar
 } = require('../middleware/validation')
 //User routes
 Router.post('/registerNewUser', registerValidation, register)
 Router.post('/login', logInValidation, logIn)
 Router.get('/getAllMembers', getAllUsers)
 Router.get('/getUser/:name', getSingleUser)
+//change/update something
+Router.post('/changeUsername', validateUsername, changeUsername)
+Router.post('/changePassword', validatePassword, changePassword)
+Router.post('/changeAvatar', validateAvatar, changeAvatar)
 //messaging routes
-Router.post('/messages', sendPublicMessage)
-Router.get('/messages', getPublicMessage)
+// Router.post('/messages', sendPublicMessage)
+// Router.get('/messages', getPublicMessage)
 // Router.post('/send-message', sendMessageToUser)
 // Router.get('/messages/:senderName/', receiveMessageFromUser)
 module.exports = Router
